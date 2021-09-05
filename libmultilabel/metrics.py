322ceaf8468c99d4f8ee9c7504c9ce7af95dc725
Tim as short <49252667+Abhinav43@users.noreply.github.com>
2021-09-05 15:58:03 +0530

    Update metrics.py

--- libmultilabel/metrics.py
+++ libmultilabel/metrics.py
@@ -2,7 +2,7 @@ import re
 
 import torch
 import numpy as np
-from torchmetrics import Metric, MetricCollection, F1, Precision, Recall, RetrievalNormalizedDCG
+from torchmetrics import Metric, MetricCollection, F1, Precision, Recall, RetrievalNormalizedDCG, AUROC, AUC
 from torchmetrics.utilities.data import select_topk
 
 
@@ -10,7 +10,6 @@ class RPrecision(Metric):
     """R-precision calculates precision at k by adjusting k to the minimum value of the number of
     relevant labels and k. Please find the definition here:
     https://nlp.stanford.edu/IR-book/html/htmledition/evaluation-of-ranked-retrieval-results-1.html
-
     Args:
         top_k (int): the top k relevant labels to evaluate.
     """
@@ -39,23 +38,43 @@ class RPrecision(Metric):
         return self.score / self.num_sample
 
 
-def get_metrics(metric_threshold, monitor_metrics, num_classes):
+def get_metrics(metric_threshold, monitor_metrics, num_classes, top_k = [5,8]):
     if monitor_metrics is None:
         monitor_metrics = []
+
+    if type(top_k) != list:
+      tok_k = [tok_k]
+
     macro_prec = Precision(num_classes, metric_threshold, average='macro')
-    macro_recall = Recall(num_classes, metric_threshold, average='macro')
+    macro_recall = Recall(num_classes, metric_threshold,  average='macro')
     another_macro_f1 = 2 * (macro_prec * macro_recall) / \
         (macro_prec + macro_recall + 1e-10)
+    
     metrics = {
-        'Micro-Precision': Precision(num_classes, metric_threshold, average='micro'),
-        'Micro-Recall': Recall(num_classes, metric_threshold, average='micro'),
-        'Micro-F1': F1(num_classes, metric_threshold, average='micro'),
-        'Macro-F1': F1(num_classes, metric_threshold, average='macro'),
-        # The f1 value of macro_precision and macro_recall. This variant of
-        # macro_f1 is less preferred but is used in some works. Please
-        # refer to Opitz et al. 2019 [https://arxiv.org/pdf/1911.03347.pdf]
-        'Another-Macro-F1': another_macro_f1,
-    }
+                    'Micro-Precision'   : Precision(num_classes, metric_threshold, average='micro'),
+                    'Micro-Recall'.     : Recall(num_classes, metric_threshold, average='micro'),
+                    'Micro-F1'          : F1(num_classes, metric_threshold, average='micro'),
+                    'Macro-F1'          : F1(num_classes, metric_threshold, average='macro'),
+                    'Another-Macro-F1'  : another_macro_f1, 
+                    'AUC-ROC macro'     : AUROC(num_classes, average='macro'), 
+                    'AUC-ROC micro'     : AUROC(num_classes, average='micro')
+              }
+
+
+    macr_pre = {f'Micro-Precision @ {k}': Precision(num_classes, 
+                                                    metric_threshold, 
+                                                    average='micro', 
+                                                    top_k = k) for k in top_k}
+
+    micr_pre = {f'Macro-Precision @ {k}': Precision(num_classes, 
+                                                    metric_threshold, 
+                                                    average='macro', 
+                                                    top_k = k) for k in top_k}
+
+    metrics.update(macr_pre)
+    metrics.update(micr_pre)
+
+
     for metric in monitor_metrics:
         if isinstance(metric, Metric):  # customized metric
             metrics[type(metric).__name__] = metric
@@ -70,7 +89,9 @@ def get_metrics(metric_threshold, monitor_metrics, num_classes):
         elif re.match('nDCG@\d+', metric):
             metrics[metric] = RetrievalNormalizedDCG(k=int(metric[5:]))
 
-        elif metric not in ['Micro-Precision', 'Micro-Recall', 'Micro-F1', 'Macro-F1', 'Another-Macro-F1']:
+        elif metric not in ['Micro-Precision', 'Micro-Recall', 'Micro-F1', 
+                            'Macro-F1', 'Another-Macro-F1', 'AUC-ROC macro', 
+                            'AUC-ROC micro']:
             raise ValueError(f'Invalid metric: {metric}')
 
     return MetricCollection(metrics)
